804a61095a52dcfec779e1960d2c2a9914c5a527
akhila <bakhila36@gmail.com>
2019-08-03 23:15:01 +0530

    issues fixed

--- src/main/java/com/stackroute/muzix/config/SwaggerConfig.java
+++ src/main/java/com/stackroute/muzix/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    @Bean
+    @Bean // indicates that a method produces a bean to be managed by Spring.
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors
